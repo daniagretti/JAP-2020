4c685f250557aded8053e3a7055180fba0d4abe5
daniagretti <68909802+daniagretti@users.noreply.github.com>
2020-08-27 10:21:43 -0300

    Update login.js
    guardar los datos del usuario en el local storage

--- js/login.js
+++ js/login.js
@@ -2,5 +2,16 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-
+//que cuando se haga click en el boton para el login guarde los datos
+    document.getElementById("botonSignIn").addEventListener("click", function(){
+        loginDatosDeUsuario();
+    });
 });
+function loginDatosDeUsuario (){
+    let userName = document.getElementById("idUser").value;
+    let pasword = document.getElementById("idPasword").value;
+    //creo un JSON para luego poder usar como objeto parseando cuando hago el getItem
+    var usuarioActual = ` { "userName":"`+ userName +`", "pasword":"`+ pasword + `"} `;
+    window.localStorage.clear();
+    window.localStorage.setItem("usuarioActual", usuarioActual);
+}

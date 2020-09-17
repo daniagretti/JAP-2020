dda4a33e2915184512cd313d2c8060c8026b45f1
daniagretti <68909802+daniagretti@users.noreply.github.com>
2020-09-17 10:42:57 -0300

    Update product-info.js
    agregue:
    -comentarios
    -carrusel de imagenes
    -cambie formato de productos relacionados
    -que se limpie donde escribir los comentarios luego de enviar el comentario

--- js/product-info.js
+++ js/product-info.js
@@ -5,18 +5,29 @@ var rating = 0;
 
 function showImagesGallery(array) {
     let htmlImg = "";
+    let htmlCarousel = "";
     for (let i = 0; i < array.length; i++) {
         let imagenes = array[i];
-        htmlImg += `<div class="col-lg-3 col-md-4 col-6">
-        <div class="d-block mb-4 h-100">
-        <img  class="img-fluid img-thumbnail" src="`+ imagenes + `"alt=""> 
-        </div>
-        </div>`
-        document.getElementById("productImagesGallery").innerHTML = htmlImg;
+        if (i == 0) {
+            //para poner el que esta activo
+            htmlImg += `<li data-target="#carouselExampleIndicators" data-slide-to="` + i + `" class="active"></li>`
+            htmlCarousel += `
+                <div class="carousel-item active" data-interval="5000">
+                    <img src="` + imagenes + `" class="d-block w-100" alt="...">
+                </div> `
+        } else {
+            //para poner el resto
+            htmlImg += `<li data-target="#carouselExampleIndicators" data-slide-to=" ` + i + `"></li> `
+            htmlCarousel += `
+                <div class="carousel-item" data-interval="2000">
+                    <img src="`+ imagenes + `" class="d-block w-100" alt="...">
+                </div>`
+        }
     }
-
-
+    document.getElementById("carrusel").innerHTML = htmlImg; //ponemos la cantidad de li dentro del ol en html para que muestre la cantidad de imagenes
+    document.getElementById("productImages").innerHTML = htmlCarousel; //agregamos el carrusel propiamente dicho
 }
+
 
 function showRelatedProducts(array) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
@@ -24,14 +35,14 @@ function showRelatedProducts(array) {
             prodAll = resultObj.data;
             let htmlRelatedProds = "";
             for (let i = 0; i < array.length; i++) {
-                let related = prodAll[array[i]];
+                let related = prodAll[array[i]];  
                 htmlRelatedProds += `
-                <div class= "col-lg-3 col-md-4 col-6 border">
+                <div class= "col-lg-3 col-md-4 col-6 border" id="related">
                     <div id="relatedProdImg" class= "row">
                         <img class="img-fluid p-2" src="`+ related.imgSrc + `">                                              
                     </div>                   
                     <div "relatedProdInfo" class= "row p-2">
-                    <p>`+ related.name + `</p> 
+                     <strong style="color: rgb(245, 21, 107);">`+ related.name + `</strong> 
                     <p>`+ related.description + `</p>
                     </div>
                     <div class= "row p-2">
@@ -132,14 +143,15 @@ getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
 
         }
     });
-    document.getElementById("enviar").addEventListener("click", function(){
+    document.getElementById("enviar").addEventListener("click", function(){ //cuando se hace click en enviar en la seccion de comentar
         
-        let texto = document.getElementById("textArea").value;
+        let texto = document.getElementById("textArea").value; //tomar lo que esta comentado para luego mostrarlo
+        document.getElementById("textAreaComment").value = ""; //para limpiar la caja de texto
         let user = JSON.parse(localStorage.getItem("usuarioActual")); //para obtener el nombre del usuario ingresado
    
-
+        //para hacer la fecha en el comentario en tiempo real
         let today = new Date();
-        let mes = parseInt(today.getMonth()+1);
+        let mes = parseInt(today.getMonth()+1); //hay que sumarle uno porque arranca del 0
 
         //para que queden con un 0 adelante, 01...09
         if(mes<10){
